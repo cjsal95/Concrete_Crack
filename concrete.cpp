db397110d1a857472539ef6777ab9157a17797cb
cjsal95 <cjsal95@naver.com>
2017-08-01 15:09:20 +0900

    Add files via upload

--- concrete.cpp
+++ concrete.cpp
@@ -17,7 +17,8 @@ int main() {
 	Mat img, sobelX, sobelY;
 	Mat dstX, dstX2, dstY, dstY2;
 	
-	img = imread("cement_cracked (94).jpg", CV_8U);   
+	//img = imread("cement_cracked (590).jpg", CV_8U);  
+	img = imread("test.jpg", CV_8U);   
 
 	if(img.empty()) {
 		cout<<"Error reading image"<<endl;
@@ -27,8 +28,7 @@ int main() {
 	Sobel(img, sobelX, CV_8U, 1, 0, 3);   // 수평
 	Sobel(img, sobelY, CV_8U, 0, 1, 3);   // 수직
 
-	imshow("sobelX", sobelX);
-	imshow("sobelY", sobelY);
+	
 
 	Mat sobelX2 = sobelX.clone(), sobelX3 = sobelX.clone(), sobelX4 = sobelX.clone();
 	Mat sobelY2 = sobelY.clone(), sobelY3 = sobelY.clone(), sobelY4 = sobelY.clone();
@@ -37,22 +37,31 @@ int main() {
 	NF1(sobelX);
 	NF1(sobelY);
 
-	NF2(sobelX2, 10, 2, 13);    // 수평
-	NF2(sobelY2, 2, 10, 13);    // 수직
+	imshow("sobelX", sobelX);
+
+
+	NF2(sobelX2, 10, 2, 9);    // 수평
+	NF2(sobelY2, 2, 10, 8);    // 수직
 
 	imshow("NF2X", sobelX2);
-	imshow("NF2Y", sobelY2);
+	//imshow("NF2Y", sobelY2);
 
 
 	// Noise filtering 3
-	NF2(sobelX3, 4, 4, 12);
-	NF2(sobelY3, 4, 4, 12);
+	NF2(sobelX3, 4, 4, 7);
+	NF2(sobelY3, 4, 4, 7);
 
-	//imshow("sobelX3", sobelX3);
+	imshow("sobelX3", sobelX3);
 	//imshow("sobelY3", sobelY3);
 
-	NF4(sobelX4, 0);
-	NF4(sobelY4, 0);
+	NF4(sobelX4, 30);
+	NF4(sobelY4, 60);
+
+	imshow("sobelX4", sobelX4);
+	//imshow("sobelY4", sobelY4);
+
+	//imwrite("sobelX4.png", sobelX4);
+	//imwrite("sobelY4.png", sobelY4);
 	
 	
 	// 수평 성분끼리 합치기
@@ -68,7 +77,7 @@ int main() {
 
 	
 	//imshow("dstY2", dstY);
-	//imshow("dstX2", dstX);
+	//imshow("dstX", dstX);
 
 	
 	Mat result(Size(dstX.rows, dstX.cols), CV_8U);
@@ -87,23 +96,28 @@ int main() {
 	}
 	*/
 	
+	
+
 	for(int x = 0; x < dstX.rows; x++) {
 		for(int y = 0; y < dstX.cols; y++) {
 			
-			if(dstX.at<uchar>(x, y) > 127 && dstY.at<uchar>(x, y) > 127)
+			if(dstX.at<uchar>(x, y) > 200 && dstY.at<uchar>(x, y) > 200)
 				result.at<uchar>(x, y) = 255;
 				
-			else if(dstX.at<uchar>(x, y) < 127 && dstY.at<uchar>(x, y) < 127)
+			else if(dstX.at<uchar>(x, y) < 200 && dstY.at<uchar>(x, y) < 200)
 				result.at<uchar>(x, y) = 0;
 				
 			else
-				result.at<uchar>(x, y) = 50;
+				result.at<uchar>(x, y) = 100;
 		}
 	}
 	
 	bitwise_not(result, result);   // 반전
 
 	imshow("result", result);
+
+	imwrite("result.png", result);
+	//imwrite("sobelY4.png", sobelY4);
 	
 	namedWindow("Original Image");
 	imshow("Original Image", img);
@@ -121,7 +135,7 @@ Mat NF1(Mat image) {
 	for(i = 0; i < image.cols; i++) {                      
 		for(j = 0; j < image.rows; j++) {
 
-			if(image.at<uchar>(j, i) < 180) 
+			if(image.at<uchar>(j, i) < 100) 
 				image.at<uchar>(j, i) = 0;
 			
 		}
@@ -203,21 +217,21 @@ Mat NF2(Mat image, int matX, int matY, int noise) {
 	for(int x = 0; x <= image.rows - matX; x++) {
 		for(int y = 0; y <= image.cols - matY; y++) {
 
-			Mat ROI(image, Rect(y, x, matY, matX));       // 잡음은 0이 아니다. 17개 이하일 경우 노이즈로 간주하여 제거한다.
+			Mat ROI(image, Rect(y, x, matY, matX));       
 
 			nos = 0;
 			
 			for(m =0; m < matX; m++) {
 				for(n = 0; n < matY; n++) {
 					
-					if(ROI.at<uchar>(m, n) > 0)        // 해당 픽셀에 값이 잡음일 경우 noise++
+					if(ROI.at<uchar>(m, n) > 50)        // 해당 픽셀에 값이 잡음일 경우 noise++
 						nos++;
 				}
 			}
 
 			//printf("nos = %d\n", nos);
 
-			if(nos < noise) {                             // noise가 3보다 작을 경우 모든 픽셀을 검정으로
+			if(nos < noise) {                             
 				
 				for(m =0; m < matX; m++) {
 					for(n = 0; n < matY; n++) {
